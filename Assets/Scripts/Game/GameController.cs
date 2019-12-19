856132ef84baad0e3e1fc3fab0104d7bae8a0f28
xiaxiaohu <xxh@g3games.cn>
2019-12-19 18:04:25 +0800

    init

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] gos;
+
+    public Rigidbody rigibody;
+
+	// Use this for initialization
+	void Start () {
+
+        rigibody.AddRelativeForce(new Vector3(300,300,0));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
