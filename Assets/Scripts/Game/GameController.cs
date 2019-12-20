c2dd19a03872edbf16de567f25ea170546d7845c
xiaxiaohu <xxh@g3games.cn>
2019-12-20 18:21:33 +0800

    1

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -3,19 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+    public LineRenderer line;
     [SerializeField]
     private GameObject[] gos;
 
     public Rigidbody rigibody;
+    private Vector3 startV3;
+    private Vector3 moveV3;
+    private MovePath movePath;
 
-	// Use this for initialization
-	void Start () {
-
-        rigibody.AddRelativeForce(new Vector3(300,300,0));
+    // Use this for initialization
+    void Start () {
+        movePath = new MovePath();
+        movePath.startPosition = rigibody.gameObject.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0))
+        {
+            movePath.screenPosition = Input.mousePosition;
+            movePath.ready();
+            RrawLine();
+        }
 	}
+
+    void RrawLine()
+    {
+        Vector3[] v3 = new Vector3[21];
+        v3[0] = movePath.startPosition;
+        for (int i = 0; i < 20; i++)
+        {
+            float t = i * Time.fixedDeltaTime;
+            v3[i + 1] = movePath.pathPosition(t);
+        }
+        line.SetPositions(v3);
+        tt = 0;
+        startMove = true;
+    }
+
+    private bool startMove;
+    private float tt;
+    void FixedUpdate()
+    {
+        //if (startMove)
+        //{
+        //    tt += Time.fixedDeltaTime;
+        //    Vector3 v3 = startV3;
+        //    v3.x += moveV3.x * 30 * tt + tt * tt * moveV3.x * 10;
+        //    v3.y += moveV3.y * 30 * tt + tt * tt * -20;
+        //    rigibody.gameObject.transform.position = v3;
+        //}
+    }
+
+    
 }
---
+++ Assets/Scripts/Game/MovePath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePath
+{
+    public Vector2 startMove = new Vector2(30, 30);
+    public Vector2 acceleration = new Vector2(10, -20);
+    public Vector3 startPosition { get; set; }
+    public Vector3 endPosition { get; set; }
+    private Vector3 direction;
+
+    public Vector3 screenPosition {
+        set
+        {
+            Debug.Log(value);
+            Vector3 v3 = Camera.main.ScreenToWorldPoint(value);
+            v3.z = 0;
+            endPosition = v3;
+            Debug.Log(endPosition);
+        }
+    }
+
+
+    public void ready()
+    {
+        direction = Vector3.Normalize(endPosition - startPosition);
+    }
+
+    public Vector3 pathPosition(float time)
+    {
+        Vector3 v3 = startPosition;
+        v3.x += direction.x * startMove.x * time + time * time * direction.x * acceleration.x;
+        v3.y += direction.y * startMove.y * time + time * time * acceleration.y;
+        return v3;
+    }
+}
